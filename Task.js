f9e3ca00a14ed43e8ad69e8a52213444d97783d3
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-04-10 01:43:03 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -132,7 +132,7 @@ cron "6 9 * * *" script-path=https://raw.githubusercontent.com/id77/QuantumultX/
 
 # >机场签到
 # boxjs订阅：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
-cron "25 22 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkin_env.js, tag=机场签到
+cron "40 23 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkin_env.js, tag=机场签到
 
 # >Boxjs
 # 抹掉全局备份
