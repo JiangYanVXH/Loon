2b92ce51c1a42530aa4e0b8516622bf47931d620
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-05-17 19:58:55 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -121,10 +121,10 @@ cron "12 22 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdreso
 # 京东物流派件提醒
 cron "6 9 * * *" script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/task/jdWuLiu.js , enable=false, tag=京东物流派件提醒
 
-# 半点红包雨
+# 半点京豆雨
 cron "15 30 16-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_half_redrain.js,tag=半点京豆雨
-# 整点红包雨（12 0 8-23/1 * * *）
-cron "15 0 8-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js, enable=true, tag=整点京豆红包雨
+# 整点京豆雨（12 0 8-23/1 * * *）
+cron "15 0 8-23/1 * * *" script-path=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/jd_super_redrain.js, enable=true, tag=整点京豆雨
 # 直播间抽奖
 cron "1 30 20 */2 * *" script-path=https://raw.githubusercontent.com/xbt370/Collection/master/Script/jd_live_lottery_social.js, enable=false, tag=直播间抽奖
 # 超级直播间
