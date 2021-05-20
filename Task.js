b18f320874fed006be803cade083876a37473d2c
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-05-20 21:58:00 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -6,6 +6,9 @@ cron "24 8,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script
 
 # > 京东APP
 
+# 人头马x博朗
+cron "20 9 20-31 5 *" script-path=https://raw.githubusercontent.com/monk-coder/dust/dust/normal/adolf_martin.js,tag=人头马x博朗
+
 # 每日抽奖
 # cron "25 1,22 1-31 5 *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_daily_lottery.js, tag=每日抽奖
 
