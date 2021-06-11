3c1fbf0988816216b7a2dd5dfd0cffd0b4a52c80
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-06-11 15:40:49 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -4,6 +4,9 @@ cron "8 30 16-23/1 * * *" script-path=https://raw.githubusercontent.com/longzhuz
 # 整点京豆雨
 cron "8 0 8-23/1 * * *" script-path=https://raw.githubusercontent.com/longzhuzhu/nianyu/main/qx/long_super_redrain.js, enable=false, tag=整点京豆雨
 
+# 获取京东互助码
+cron "12 22 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_get_share_code.js, enable=false, tag=获取互助码
+
 # >机场签到
 # boxjs订阅：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
 cron "32 21 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkin_env.js, tag=机场签到
