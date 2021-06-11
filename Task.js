cac483e5bfc87953a949103c43519ce97f4695d7
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-06-11 15:42:09 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,3 +1,6 @@
+# 宠汪汪积分兑换奖品
+cron "3,5 0 0-16/8 * * *" script-path=https://raw.githubusercontent.com/hyzaw/scripts/main/backup/ddo_joy_reward.js, enable=false, tag=宠汪汪积分兑换奖品
+
 # 半点京豆雨
 cron "8 30 16-23/1 * * *" script-path=https://raw.githubusercontent.com/longzhuzhu/nianyu/main/qx/long_half_redrain.js, enable=false, tag=半点京豆雨
 
