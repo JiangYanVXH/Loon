1a5202b7edad9dabf0641ee9e7540790ca0bfd67
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-06-11 15:48:21 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -22,3 +22,9 @@ cron "12 22 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdreso
 # >机场签到
 # boxjs订阅：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
 cron "32 21 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/checkin_env.js, tag=机场签到
+
+# > 叮咚买菜
+# 叮咚买菜签到
+cron "28 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js,tag=叮咚买菜
+# 叮咚农场养鱼
+cron "24 8,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js,tag=叮咚养鱼
