018792678ce635b8edac18cec0ea32101e788d9e
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-10-06 16:50:41 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -24,6 +24,7 @@ cron "12 22 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd
 cron "0 0-18/6 * * *" script-path=https://raw.githubusercontent.com/smiek2221/scripts/master/gua_carnivalcity.js, tag=手机狂欢城
 cron "24 7,10 * * *" script-path=https://raw.githubusercontent.com/smiek2221/scripts/master/gua_MMdou.js, tag=升级赚京豆
 cron "15 3,9 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_ddworld.js,tag=东东世界
+cron "55 6,9 * 10 *" script-path=https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_nzmh.js, tag=女装盲盒
 
 # >机场签到
 # boxjs订阅：https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/evilbutcher.boxjs.json
