da7d0619ae128ea75470033c1a601df4b5cf117f
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-10-31 16:18:00 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,8 +1,8 @@
-cron "58 1,8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_api_test.js, tag=互助码环境测试（通用版）
-#cron "10 2,21 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_star_shop.js, tag=明星小店/星店长（通用版）
+cron "58 1,8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_api_test.js, enabled=false, tag=互助码环境测试（通用版）
+#cron "10 2,21 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_star_shop.js, enabled=false, tag=明星小店/星店长（通用版）
 cron "17 8,10,13 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_syj.js, tag=赚京豆（通用版）
 cron "28 1 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_jin_tie.js, tag=领金贴（通用版）
-cron "44 9-23/8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_superMarket.js, tag=东东超市（通用版）
+cron "44 9-23/8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_superMarket.js, enabled=false, tag=东东超市（通用版）
 cron "30,50 0,1 0 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_shop.js, tag=进店领豆（通用版）
 cron "20 8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_sgmh.js, tag=闪购盲盒（通用版）
 cron "1 7-21/2 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_plantBean.js, tag=种豆得豆（通用版）
@@ -16,14 +16,14 @@ cron "36 6-19/6 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorl
 cron "6 3,13 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_club_lottery.js, tag=摇京豆（通用版）
 cron "38 0-22/5 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_cash.js, tag=签到领现金（通用版）
 cron "12 7 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_bean_home.js, tag=领京豆额外奖励（通用版）
-cron "12 */8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_health.js, tag=东东健康社区（通用版）
-cron "42 10 1-23/3 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_health_collect.js, tag=东东健康社区收集能量（通用版）
+cron "12 */8 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_health.js, enabled=false, tag=东东健康社区（通用版）
+cron "42 10 1-23/3 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_health_collect.js, enabled=false, tag=东东健康社区收集能量（通用版）
 cron "33 12,21 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_unsubscribe.js, tag=取关京东店铺商品
 cron "12 22 * * *" script-path=https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_get_share_code.js, enabled=false, tag=获取京东互助码
 
 # >其他
-cron "50 0-18/6 * * *" script-path=https://raw.githubusercontent.com/smiek2221/scripts/master/gua_carnivalcity.js, tag=手机狂欢城
-cron "50 0-18/6 * * *" script-path=https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_carnivalcity.js , tag=手机狂欢城2
+cron "50 0-18/6 1-12 11 *" script-path=https://raw.githubusercontent.com/smiek2221/scripts/master/gua_carnivalcity.js, tag=手机狂欢城
+cron "50 0-18/6 * * *" script-path=https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_carnivalcity.js, enabled=false, tag=手机狂欢城2
 cron "24 7,10 * * *" script-path=https://raw.githubusercontent.com/smiek2221/scripts/master/gua_MMdou.js, tag=升级赚京豆
 cron "15 3,9 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_ddworld.js,tag=东东世界
 cron "55 6,9 * 10 *" script-path=https://raw.githubusercontent.com/shufflewzc/faker2/main/jd_nzmh.js, tag=女装盲盒
@@ -35,4 +35,4 @@ cron "32 21 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/Qua
 # >叮咚买菜
 cron "28 8,17 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js,tag=叮咚买菜
 cron "24 8,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js,tag=叮咚养鱼
-cron "22 8,17 * * *" script-path= https://raw.githubusercontent.com/iepngs/Script/master/dingdong/ddgy.js,tag=叮咚果园
+cron "22 8,17 * * *" script-path= https://raw.githubusercontent.com/iepngs/Script/master/dingdong/ddgy.js, enabled=false,tag=叮咚果园
